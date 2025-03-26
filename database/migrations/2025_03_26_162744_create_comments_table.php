632a9f9c086cb547467dc26cdd9f17358c790cbd
DanDev <danielcrd1216@gmail.com>
2025-03-26 17:35:46 +0100

    Migrations

--- database/migrations/2025_03_26_162744_create_comments_table.php
+++ database/migrations/2025_03_26_162744_create_comments_table.php
@@ -11,13 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('comments', function (Blueprint $table) {
             $table->id();
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->string('name')->nullable();
-            $table->string('avatar')->nullable();
-            $table->string('status')->default('active');
+            $table->foreignId('post_id')->constrained()->onDelete('cascade');
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->text('comment');
             $table->timestamps();
         });
     }
@@ -27,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('comments');
     }
 };
